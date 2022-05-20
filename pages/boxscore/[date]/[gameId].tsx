a616270dd0b0ef9e2d2d72d4938676cac897a730
Brian West <WestBrian@users.noreply.github.com>
2022-05-19 22:50:04 -0400

    feat: add responsive designs for all pages (#2)

--- pages/boxscore/[date]/[gameId].tsx
+++ pages/boxscore/[date]/[gameId].tsx
@@ -1,27 +1,8 @@
-import type { GetServerSideProps, NextPage } from 'next'
+import type { NextPage } from 'next'
 import { Boxscore, BoxscoreProps } from '../../../src/pages/Boxscore'
-import { nbaService } from '../../../src/services/nba.service'
-import { parse } from 'date-fns'
 
-const BoxscorePage: NextPage<BoxscoreProps> = ({ boxscore }) => {
-  return <Boxscore boxscore={boxscore} />
-}
-
-export const getServerSideProps: GetServerSideProps<
-  BoxscoreProps,
-  { date: string; gameId: string }
-> = async (context) => {
-  const { date, gameId } = context.params!
-  const boxscore = await nbaService.getBoxscore(
-    parse(date, 'yyyyMMdd', new Date()),
-    gameId
-  )
-
-  return {
-    props: {
-      boxscore
-    }
-  }
+const BoxscorePage: NextPage<BoxscoreProps> = () => {
+  return <Boxscore />
 }
 
 export default BoxscorePage
--- src/components/boxscore-table/index.tsx
+++ src/components/boxscore-table/index.tsx
@@ -7,18 +7,27 @@ import {
   Tr,
   Th,
   Td,
-  useColorMode
+  VStack,
+  Text,
+  useColorMode,
+  useBreakpoint
 } from '@chakra-ui/react'
 import type { IBoxscore } from '../../types'
 
+const mobileHeaders = ['name', 'reb', 'ast', 'pts']
 const headers = ['name', 'min', 'fg', '3pt', 'ft', 'reb', 'ast', 'pts', '+/-']
 
 export interface BoxscoreTableProps {
+  teamName: string
   playerStats: NonNullable<IBoxscore['stats']>['activePlayers']
 }
 
-export const BoxscoreTable: FC<BoxscoreTableProps> = ({ playerStats }) => {
+export const BoxscoreTable: FC<BoxscoreTableProps> = ({
+  teamName,
+  playerStats
+}) => {
   const { colorMode } = useColorMode()
+  const breakpoint = useBreakpoint('md')
 
   function formatName(firstName: string, lastName: string) {
     return `${firstName[0]} ${lastName}`
@@ -29,48 +38,67 @@ export const BoxscoreTable: FC<BoxscoreTableProps> = ({ playerStats }) => {
     return arr[0]
   }
 
+  function formatPlusMinus(plusMinus: string) {
+    if (plusMinus === '0' || plusMinus.includes('-') || plusMinus === '') {
+      return plusMinus
+    } else {
+      return `+${plusMinus}`
+    }
+  }
+
   return (
-    <Box
-      bg={colorMode === 'light' ? 'gray.300' : 'gray.900'}
-      borderRadius={'md'}
-      width={'full'}
-    >
-      <Table size={'sm'} variant={'simple'}>
-        <Thead>
-          <Tr>
-            {headers.map((header) => (
-              <Th key={header} isNumeric={header !== 'name'}>
-                {header}
-              </Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody>
-          {playerStats.map((player, index) => (
-            <Tr
-              key={player.personId}
-              borderBottom={index === 4 ? '8px' : undefined}
-              borderColor={colorMode === 'light' ? 'gray.100' : 'gray.700'}
-            >
-              <Td>{formatName(player.firstName, player.lastName)}</Td>
-              <Td isNumeric>{formatMinutes(player.min)}</Td>
-              <Td isNumeric>
-                {player.fgm}-{player.fga}
-              </Td>
-              <Td isNumeric>
-                {player.tpm}-{player.tpa}
-              </Td>
-              <Td isNumeric>
-                {player.ftm}-{player.fta}
-              </Td>
-              <Td isNumeric>{player.totReb}</Td>
-              <Td isNumeric>{player.assists}</Td>
-              <Td isNumeric>{player.points}</Td>
-              <Td isNumeric>{player.plusMinus}</Td>
+    <VStack spacing={2} align={'flex-start'}>
+      <Text fontWeight={'semibold'}>{teamName}</Text>
+      <Box
+        bg={colorMode === 'light' ? 'gray.300' : 'gray.900'}
+        borderRadius={'md'}
+        width={'full'}
+      >
+        <Table size={'sm'} variant={'simple'}>
+          <Thead>
+            <Tr>
+              {(breakpoint === 'base' ? mobileHeaders : headers).map(
+                (header) => (
+                  <Th key={header} isNumeric={header !== 'name'}>
+                    {header}
+                  </Th>
+                )
+              )}
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </Box>
+          </Thead>
+          <Tbody>
+            {playerStats.map((player, index) => (
+              <Tr
+                key={player.personId}
+                borderBottom={index === 4 ? '8px' : undefined}
+                borderColor={colorMode === 'light' ? 'gray.100' : 'gray.700'}
+              >
+                <Td>{formatName(player.firstName, player.lastName)}</Td>
+                {breakpoint !== 'base' && (
+                  <>
+                    <Td isNumeric>{formatMinutes(player.min)}</Td>
+                    <Td isNumeric>
+                      {player.fgm}-{player.fga}
+                    </Td>
+                    <Td isNumeric>
+                      {player.tpm}-{player.tpa}
+                    </Td>
+                    <Td isNumeric>
+                      {player.ftm}-{player.fta}
+                    </Td>
+                  </>
+                )}
+                <Td isNumeric>{player.totReb}</Td>
+                <Td isNumeric>{player.assists}</Td>
+                <Td isNumeric>{player.points}</Td>
+                {breakpoint !== 'base' && (
+                  <Td isNumeric>{formatPlusMinus(player.plusMinus)}</Td>
+                )}
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </VStack>
   )
 }
